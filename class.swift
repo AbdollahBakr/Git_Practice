b3dd6431438b1417ce8bfad4d7b187493d3c0d47
Abdollah Bakr <abdollah.bakr@gmail.com>
2022-06-11 14:06:06 +0200

    added User class with id and name in class.swift

---
+++ class.swift
@@ -0,0 +1,12 @@
+class User {
+    var id: String?
+    var name: String?
+    
+    func getUserId() -> String {
+        return self.id
+    }
+    
+    func getUserName() -> String {
+        return self.name
+    }
+}
